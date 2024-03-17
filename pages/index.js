a07bb55bb67782d5c122c21b9236df3e44365d4c
Miguel Urena <urena_miguel82@outlook.com>
2024-03-17 14:18:50 -0700

    Add components

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,8 @@
 import Head from "next/head";
 
 const posts = [
-  { title: 'React Testing', excerpt: 'Learn how to test React Components' },
-  { title: 'React With Tailwind', excerpt: 'Learn how to add Tailwind to React' }
+  { title: 'React Testing', excerpt: 'Learn React Testing' },
+  { title: 'React With Tailwind', excerpt: 'Learn React with Tailwind' }
 ];
 
 export default function Home() {
@@ -13,9 +13,20 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <div className="grid grid-cols-1 lg:grid-cols-12 gap-12">
+        <div className="lg:col-span-8 col-span-1">
+          {posts.map((post, index) => (
+            <div>
+              {post.title}
+              {post.excerpt}
+            </div>
+          ))}
+        </div>
+        <div className="lg:col-span-4 col-span-1">
+            <div className="lg:sticky relative top-8">
 
+            </div>
+        </div>
       </div>
-
     </div>
   );
 }
