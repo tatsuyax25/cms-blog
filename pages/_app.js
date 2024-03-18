0fd1b1c3898436c1e9f3bf0aa0bd0dbff498ebf4
Miguel Urena <urena_miguel82@outlook.com>
2024-03-18 15:39:52 -0700

    Add Layout component

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,12 @@
+import React, { useEffect, useState } from 'react';
+import { Layout } from '../components';
 import 'tailwindcss/tailwind.css';
-
 import "../styles/globals.scss";
 
 export default function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <Layout>
+      <Component {...pageProps} />;
+    </Layout>
+  );
 }
