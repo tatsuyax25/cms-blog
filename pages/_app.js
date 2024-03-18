20c203741a1a1075c4f39131f70cc46d32b9dc35
Miguel Urena <urena_miguel82@outlook.com>
2024-03-18 14:58:57 -0700

    Fix issues and add background

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,6 @@
-import "@/styles/globals.css";
+import 'tailwindcss/tailwind.css';
+
+import "../styles/globals.scss";
 
 export default function App({ Component, pageProps }) {
   return <Component {...pageProps} />;
