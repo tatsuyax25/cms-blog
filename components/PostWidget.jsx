40bd4c43a7bd48c38dd556ac5d32555955dc697a
Miguel Urena <urena_miguel82@outlook.com>
2024-04-05 17:48:16 -0700

    Fix some bugs

--- components/PostWidget.jsx
+++ components/PostWidget.jsx
@@ -9,23 +9,39 @@ const PostWidget = ({ categories, slug }) => {
 
   useEffect(() => {
     if (slug) {
-      getSimilarPosts(categories, slug)
-        .then((result) => setRelatedPosts(result))
+      getSimilarPosts(categories, slug).then((result) => {
+        setRelatedPosts(result);
+      });
     } else {
-      getRecentPosts()
-        .then((result) => setRelatedPosts(result))
+      getRecentPosts().then((result) => {
+        setRelatedPosts(result);
+      });
     }
-  }, [slug])
-
-  console.log(relatedPosts)
+  }, [slug]);
 
   return (
     <div className="bg-white shadow-lg rounded-lg p-8 mb-8">
       <h3 className="text-xl mb-8 font-semibold border-b pb-4">
         {slug ? 'Related Posts' : 'Recent Posts'}
       </h3>
+      {relatedPosts.map((post) => (
+        <div key={post.title} className="flex items-center w-full mb-4">
+          <div className="w-16 flex-none">
+            <img 
+              alt={post.title}
+              height="60px"
+              width="60px"
+              className="align-middle rounded-full"
+              src={post.featuredImage.url}
+            />
+          </div>
+          <div className="flex-grow ml-4">
+
+          </div>
+        </div>
+      ))}
     </div>
-  )
-}
+  );
+};
 
 export default PostWidget
