9191a2a301ecc8a1841add13fb9b939435c0a497
Miguel Urena <urena_miguel82@outlook.com>
2024-04-12 12:36:05 -0700

    Working on Post

---
+++ pages/post/[slug].js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+import { getPosts, getPostDetails } from "../../services";
+
+import { PostDetail, Categories, PostWidget, Author, Comments, CommentsForm } from "../../components";
+
+const PostDetails = () => {
+  return (
+    <div>
+      PostDetails
+    </div>
+  )
+}
+
+export default PostDetails;
