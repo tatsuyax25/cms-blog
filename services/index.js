27017e448589b6dead15c647c4d2ded3d2b56869
Miguel Urena <urena_miguel82@outlook.com>
2024-04-12 11:58:08 -0700

    Get Categories

--- services/index.js
+++ services/index.js
@@ -85,3 +85,18 @@ export const getSimilarPosts = async () => {
 
   return result.post;
 }
+
+export const getCategories = async () => {
+  const query = gql`
+    query GetCategories {
+      categories: {
+        name
+        slug
+      }
+    }
+  `;
+
+  const result = await request(graphqlAPI, query);
+
+  return result.categories;
+}
