34367dbf5de820b0ff5ca94e0aa1ad4a34f5c188
Miguel Urena <urena_miguel82@outlook.com>
2024-04-17 13:43:55 -0700

    Working on Post Detail

--- pages/post/[slug].js
+++ pages/post/[slug].js
@@ -1,7 +1,6 @@
 import React from 'react'
 
 import { getPosts, getPostDetails } from "../../services";
-
 import { PostDetail, Categories, PostWidget, Author, Comments, CommentsForm } from "../../components";
 
 const PostDetails = ({ post }) => {
