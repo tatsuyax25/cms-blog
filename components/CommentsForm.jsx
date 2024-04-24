509810c380bcd6b3ae746c668ae25394b63cbebb
Miguel Urena <urena_miguel82@outlook.com>
2024-04-24 13:46:20 -0700

    Still working on Comments Form

--- components/CommentsForm.jsx
+++ components/CommentsForm.jsx
@@ -10,7 +10,27 @@ const CommentsForm = ({ slug }) => {
   const storeDataEl = useRef();
 
   const handleCommentSubmission = () => {
+    setError(false);
 
+    const { value: comment } = commentEl.current;
+    const { value: name } = nameEl.current;
+    const { value: email } = emailEl.current;
+    const { checked: storeData } = storeDataEl.current;
+
+    if (!comment || !name || !email) {
+      setError(true);
+      return;
+    }
+
+    const commentObj = { name, email, comment, slug };
+
+    if (storeData) {
+      localStorage.setItem("name", name);
+      localStorage.setItem("email", email);
+    } else {
+      localStorage.removeItem("name", name);
+      localStorage.removeItem("email", email);
+    }
   }
 
   return (
@@ -40,15 +60,17 @@ const CommentsForm = ({ slug }) => {
           name="email"
         />
       </div>
+      <div className="grid grid-cols-1 gap-4 mb-4">
+        <div>
+          <input ref={storeDataEl} type="checkbox" id="storeData" name="storeData" value="true"} />
+          <label className="text-gray-500 cursor-pointer ml-2" htmlFor="storeData">Save my name, email in this browser for the next time I comment.</label>
+        </div>
+      </div>
       {error && <p className="text-xs text-red-500">All fields are required.</p>}
       <div className="mt-8>
-        <button 
-          type="button" 
-          onClick={handleCommentSubmission}
-          className="transition duration-500 ease hover:bg-indigo-50 inline-block bg-pink-600 text-lg rounded-full text-white px-8 py-3 cursor-pointer"
-        >
-        Post Comment
-        </button>
+        <button type="button" onClick={handleCommentSubmission} className="transition duration-500 ease hover:bg-indigo-900 inline-block bg-pink-600 text-lg font-medium rounded-full text-white px-8 py-3 cursor-pointer">
+        Post Comment</button>
+        {showSuccessMessage && <span className="text-xl float-right font-semibold mt-3 text-green-500">Comment submitted for review</span>}
       </div>
     </div>
   );
