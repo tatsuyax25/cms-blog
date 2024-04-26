f239020e3e357181c11a949fd16a0e360929b157
Miguel Urena <urena_miguel82@outlook.com>
2024-04-26 12:06:56 -0700

    Working in comments.js

--- pages/api/comments.js
+++ pages/api/comments.js
@@ -4,6 +4,23 @@
 * will be treated as an API endpoint instead of a page.         *
 *************************************************************** */
 
-export default function handler(req, res) {
-  res.status(200).json({ name: "John Doe" });
+import { GraphQLClient, gql } from "graphql";
+
+const graphqlAPI = process.env.NEXT_PUBLIC_GRAPHCMS_ENDPOINT;
+
+export default function comments(req, res) {
+  const { name, email, comment, slug } = req.body;
+  const graphQLClient = new GraphQLClient(graphqlAPI, {
+    headers: {
+      authorization: `Bearer ${process.env.GRAPHCMS_TOKEN}`,
+    },
+  });
+
+  const query = gql`
+    mutation CreateComment($name: String!, $email: String!, $comment: String!, $slug: String!) {
+      createComment(data: { name: $name, email: $email, comment: $comment, post: {connect: {slug: $slug} } }) { id }
+    }
+  `
+
+  const result = await graphQLClient.request(query, req.body);
 }
