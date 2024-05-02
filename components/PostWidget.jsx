d30c4789b8dc5ae82811f3e123cbe5f89177126c
Miguel Urena <urena_miguel82@outlook.com>
2024-05-02 13:52:35 -0700

    Fixing bug

--- components/PostWidget.jsx
+++ components/PostWidget.jsx
@@ -20,17 +20,19 @@ const PostWidget = ({ categories, slug }) => {
   }, [slug]);
 
   return (
-    <div className="bg-white shadow-lg rounded-lg p-8 mb-8">
+    <div className="bg-white shadow-lg rounded-lg p-8 pb-12 mb-8">
       <h3 className="text-xl mb-8 font-semibold border-b pb-4">
         {slug ? "Related Posts" : "Recent Posts"}
       </h3>
       {relatedPosts.map((post, index) => (
         <div key={index} className="flex items-center w-full mb-4">
           <div className="w-16 flex-none">
-            <img
+            <Image
+              loader={grpahCMSImageLoader}
               alt={post.title}
               height="60px"
               width="60px"
+              unoptimized
               className="align-middle rounded-full"
               src={post.featuredImage.url}
             />
@@ -39,11 +41,7 @@ const PostWidget = ({ categories, slug }) => {
             <p className="text-gray-500 font-xs">
               {moment(post.createdAt).format("MMM DD, YYYY")}
             </p>
-            <Link
-              href={`/post/${post.slug}`}
-              key={post.title}
-              className="text-md"
-            >
+            <Link href={`/post/${post.slug}`} className="text-md" key={index}>
               {post.title}
             </Link>
           </div>
